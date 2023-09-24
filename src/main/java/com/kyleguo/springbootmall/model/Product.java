c1645e0ac2fe37fef21c24972d37ec7a1b6d5171
Kyle <dramounlong3@gmail.com>
2023-09-24 20:15:14 +0800

    補充 - 時間戳和時間格式說明

--- src/main/java/com/kyleguo/springbootmall/model/Product.java
+++ src/main/java/com/kyleguo/springbootmall/model/Product.java
@@ -18,4 +18,8 @@ public class Product {
     // Date類型的變數 預設使用GMT+0
     private Date createdDate;
     private Date lastModifiedDate;
+
+    // UTC = GMT+0 英國(GMT+0) 2023/3/1 13:00:00 = 台灣(GMT+8) 2023/3/1 21::00:00
+    // 英國和台灣同一個時間點下的時間戳(Timestamp)會相同 可用System.currentTimeMillis()取得
+    // timestamp => 整數，計算從UTC 1970/1/1 00:00:00 到現在為止的總秒數
 }

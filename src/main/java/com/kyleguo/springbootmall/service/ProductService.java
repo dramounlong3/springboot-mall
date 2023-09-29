3cc776e776336b517853d4e9e9b739ced589dea7
Kyle <dramounlong3@gmail.com>
2023-09-29 17:14:41 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/kyleguo/springbootmall/service/ProductService.java
+++ src/main/java/com/kyleguo/springbootmall/service/ProductService.java
@@ -6,4 +6,5 @@ import com.kyleguo.springbootmall.model.Product;
 public interface ProductService {
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }

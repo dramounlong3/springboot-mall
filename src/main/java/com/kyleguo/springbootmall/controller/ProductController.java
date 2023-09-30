f17c0f7c5ddcafcf536c66efc97e3c7990ea3107
Kyle <dramounlong3@gmail.com>
2023-09-30 19:24:37 +0800

    商品功能 - 實作查詢商品列表 (分頁 + 總比數)

--- src/main/java/com/kyleguo/springbootmall/controller/ProductController.java
+++ src/main/java/com/kyleguo/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.kyleguo.springbootmall.dto.ProductQueryParams;
 import com.kyleguo.springbootmall.dto.ProductRequest;
 import com.kyleguo.springbootmall.model.Product;
 import com.kyleguo.springbootmall.service.ProductService;
+import com.kyleguo.springbootmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public  ResponseEntity<List<Product>> getProducts(
+    public  ResponseEntity<Page<Product>> getProducts(
             // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -45,10 +46,21 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        // 取得product list
         List<Product> productList = productService.getProducts(productQueryParams);
 
+        // 取得 product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
         // productList是空的也沒關係, 因為restful url資源定義的關係, 無須確認內容是否為空
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @GetMapping("/products/{productId}")
---
+++ src/main/java/com/kyleguo/springbootmall/util/Page.java
@@ -0,0 +1,13 @@
+package com.kyleguo.springbootmall.util;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Page<T> {
+    private Integer limit;
+    private Integer offset;
+    private Integer total;
+    private List<T> results;
+}

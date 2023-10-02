ba73e0ad7f4d45df2a56641ee539aa635b191ceb
Kyle <dramounlong3@gmail.com>
2023-10-02 23:13:39 +0800

    帳號功能 - 使用MD5 加密密碼

--- src/main/java/com/kyleguo/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/kyleguo/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -38,20 +39,31 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST); //400
         }
 
+        // 使用 MD5 執行hash雜湊運算, 介紹如下註解
+        userRegisterRequest.setPassword(DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes()));
+
         //創建帳號
         return userDao.createUser(userRegisterRequest);
+
+        // MD5 執行hash雜湊運算, 雜湊值無法由加密後的值推回原本的明碼
+        // 雜湊值可能會發生碰撞(上述無法回推的原因)
+        // MD5 因為無法回推(解密), 故無嚴格定義上 不被當做"加密", 僅是雜湊
+        // 不同人使用同樣的密碼就會發生碰撞, 所以可以用Salt處理, 降低collision
     }
 
     @Override
     public User login(UserLoginRequest userLoginRequest) {
+        //檢查email
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
         if(user == null){
             log.warn("該 email {} 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST); //400
         }
 
+
+        // 將使用者輸入的登入密碼轉成MD5後再與資料庫的密碼比較
         // java比較請見下方註解說明
-        if(user.getPassword().equals(userLoginRequest.getPassword())){
+        if(user.getPassword().equals(DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes()))){
             return user;
         } else {
             log.warn("email {} 密碼不正確", userLoginRequest.getEmail());

f64ac1f015d8d7402074ff2d4f6e00a571bda652
BANK\120791 <dramounlong3@sinopac.com>
2023-10-03 20:35:12 +0800

    帳號功能 - 單元測試

---
+++ src/test/java/com/kyleguo/springbootmall/controller/UserControllerTest.java
@@ -0,0 +1,218 @@
+package com.kyleguo.springbootmall.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kyleguo.springbootmall.dao.UserDao;
+import com.kyleguo.springbootmall.dto.UserLoginRequest;
+import com.kyleguo.springbootmall.dto.UserRegisterRequest;
+import com.kyleguo.springbootmall.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    //註冊新帳號
+    @Test
+    @Transactional //因為測試使用H2 DB所以沒@Transactional也沒差
+    public void register_success() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test1@gmail.com");
+        userRegisterRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201))
+                .andExpect(jsonPath("$.userId", notNullValue()))
+                .andExpect(jsonPath("$.email", equalTo("test1@gmail.com")))
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+
+        // 檢查資料庫中的密碼不為明碼
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+        assertNotEquals(userRegisterRequest.getPassword(), user.getPassword());
+    }
+
+    @Test
+    public void register_invalidEmailFormat() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("jiwejfijie550v");
+        userRegisterRequest.setPassword("1123");
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+
+    }
+
+    @Test
+    @Transactional //因為測試使用H2 DB所以沒@Transactional也沒差
+    public void register_emailAlreadyExist() throws  Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test2@gmail.com");
+        userRegisterRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201));
+
+        // 再次使用同個 email 註冊
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+
+    }
+
+    //登入
+    @Test
+    @Transactional //因為測試使用H2 DB所以沒@Transactional也沒差
+    public void login_success() throws Exception {
+        //先註冊新帳號, 為了每個單元測試的獨立性, 所以不沿用前一個單元測試的新帳號
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test3@gmail.com");
+        userRegisterRequest.setPassword("123");
+
+
+        register(userRegisterRequest);
+
+        //再測試登入功能
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setEmail("test3@gmail.com");
+        userLoginRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.userId", notNullValue()))
+                .andExpect(jsonPath("$.email", equalTo(userRegisterRequest.getEmail()))) //test3@gmail.com
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+
+
+    }
+
+    @Test
+    @Transactional //因為測試使用H2 DB所以沒@Transactional也沒差
+    public void login_wrongPassword() throws Exception {
+        // 先註冊新帳號
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test4@gmail.com");
+        userRegisterRequest.setPassword("123");
+
+        register(userRegisterRequest);
+
+        //測試密碼輸入錯誤的情況
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setEmail("test4@gmail.com");
+        userLoginRequest.setPassword("unknown");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    @Transactional //因為測試使用H2 DB所以沒@Transactional也沒差
+    public void login_invalidEMailFormat () throws Exception {
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setEmail("j24903u0fjfio");
+        userLoginRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    @Transactional //因為測試使用H2 DB所以沒@Transactional也沒差
+    public void login_emailNotExist () throws Exception {
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        userLoginRequest.setEmail("unknown@gmail.com");
+        userLoginRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userLoginRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+    // 註冊新帳號的方法
+    private void register(UserRegisterRequest userRegisterRequest) throws Exception{
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201));
+
+
+    }
+
+
+}
